6f13ad4c44a2cb9fa69742c0bfe639c10869db29
leticiafnss <leticiafnss@gmail.com>
2026-04-17 12:54:02 -0300

    merge: integra modulo de funcionario

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,6 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [
-    // ConfigModule.forRoot(),
-
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: (config: ConfigService) => ({
-    //     type: 'mysql',
-    //     host: config.get('DB_HOST'),
-    //     port: Number(config.get('DB_PORT')),
-    //     username: config.get('DB_USER'),
-    //     password: config.get('DB_PASS'),
-    //     database: config.get('DB_NAME'),
-    //     autoLoadEntities: true,
-    //     synchronize: true,
-    //   }),
-    //   inject: [ConfigService],
-    // }),
-
-  ],
+  imports: [],
 })
 export class AppModule {}
