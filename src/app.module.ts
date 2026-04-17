07256564eac4494d946b2c385e5993e9d56eb945
Kauvinzera <kauvini2007@gmail.com>
2026-04-17 13:12:13 -0300

    feat: Modulo usuario completo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsuarioModule } from './usuario/usuario.module';
 
-@Module({
-  imports: [
-    // ConfigModule.forRoot(),
+//@Module({
+  //imports: [
+    //ConfigModule.forRoot(),
 
-    // TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   useFactory: (config: ConfigService) => ({
-    //     type: 'mysql',
-    //     host: config.get('DB_HOST'),
-    //     port: Number(config.get('DB_PORT')),
-    //     username: config.get('DB_USER'),
-    //     password: config.get('DB_PASS'),
-    //     database: config.get('DB_NAME'),
-    //     autoLoadEntities: true,
-    //     synchronize: true,
-    //   }),
-    //   inject: [ConfigService],
-    // }),
+    //TypeOrmModule.forRootAsync({
+    //imports: [ConfigModule],
+    //useFactory: (config: ConfigService) => ({
+    //type: 'mysql',
+    //host: config.get('DB_HOST'),
+    //port: Number(config.get('DB_PORT')),
+    //username: config.get('DB_USER'),
+    //password: config.get('DB_PASS'),
+    //database: config.get('DB_NAME'),
+    //autoLoadEntities: true,
+    //synchronize: true,
+    //}),
+    //inject: [ConfigService],
 
-  ],
-})
+    //}),
+    UsuarioModule
+  //],
+//})
 export class AppModule {}
